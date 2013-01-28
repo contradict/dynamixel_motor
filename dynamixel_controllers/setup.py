c61b69f0d07d8f48ef51a937a17216a3434a5865
contradict <contradict@gmail.com>
2013-01-27 18:38:26 -0800

    Fix installation of dynamixel_controller nodes
    Should be installed by cmake, not setup.py to put them in the right
    place.

--- dynamixel_controllers/setup.py
+++ dynamixel_controllers/setup.py
@@ -4,10 +4,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-           scripts=[
-               'nodes/controller_manager.py',
-               'nodes/controller_spawner.py',
-               ],
            packages=['dynamixel_controllers'],
            package_dir={'': 'src'}
           )
